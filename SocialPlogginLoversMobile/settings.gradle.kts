b3dbbe1488e52f2a0f24856e9cd4a84e3b30a3c9
DESKTOP-GJ8BP6D\Marta <marta.tristao@gmail.com>
2022-05-04 20:57:37 +0100

    app init commit

---
+++ SocialPlogginLoversMobile/settings.gradle.kts
@@ -0,0 +1,11 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "SocialPloggingLovers"
+include(":androidApp")
+include(":shared")
---
+++ SocialPlogginLoversMobile/shared/src/commonMain/kotlin/com/example/socialplogginglovers/Platform.kt
@@ -0,0 +1,5 @@
+package com.example.socialplogginglovers
+
+expect class Platform() {
+    val platform: String
+}
