e4daee10c0ed7cc9c5790ff556d09ed87866394e
DESKTOP-GJ8BP6D\Marta <marta.tristao@gmail.com>
2022-05-18 12:52:14 +0100

    dependencies for onboarding

--- SPLMobile/androidApp/build.gradle.kts
+++ SPLMobile/androidApp/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     id("com.android.application")
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
     kotlin("android")
+    id ("dagger.hilt.android.plugin")
+    id ("kotlin-kapt")
 }
 
 android {
@@ -47,16 +49,35 @@ dependencies {
     implementation ("com.google.android.gms:play-services-maps:18.0.2")
 
     //Jetpack Compose Dependencies
+    val composeVersion = "1.1.1"
         // Integration with activities
     implementation ("androidx.activity:activity-compose:1.4.0")
         // Compose Material Design
-    implementation ("androidx.compose.material:material:1.1.1")
+    implementation ("androidx.compose.material:material:$composeVersion")
         // Animations
-    implementation ("androidx.compose.animation:animation:1.1.1")
+    implementation ("androidx.compose.animation:animation:$composeVersion")
         // Tooling support (Previews, etc.)
-    implementation ("androidx.compose.ui:ui-tooling:1.1.1")
+    implementation ("androidx.compose.ui:ui-tooling:$composeVersion")
         // Integration with ViewModels
     implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.4.1")
         // UI Tests
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.1.1")
+    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:$composeVersion")
+
+    //Splash API
+    implementation("androidx.core:core-splashscreen:1.0.0-beta02")
+
+    //Navigation Compose
+    implementation("androidx.navigation:navigation-compose:2.4.2")
+
+    // Accompanist for Pager and Indicators
+    implementation("com.google.accompanist:accompanist-pager:0.23.1")
+
+    // DataStore Preferences
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
+
+    // Dagger Hilt for Dependencies Injection
+    implementation("com.google.dagger:hilt-android:2.38.1")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    kapt("com.google.dagger:hilt-android-compiler:2.38.1")
+    kapt("androidx.hilt:hilt-compiler:1.0.0")
 }
