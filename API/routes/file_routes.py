ed36e7ced4ba6260a81f44aa1b60deb2df81a377
DESKTOP-GJ8BP6D\Marta <marta.tristao@gmail.com>
2022-08-12 14:36:06 +0100

    Separate file uploading and add token required

--- API/routes/file_routes.py
+++ API/routes/file_routes.py
@@ -5,6 +5,7 @@ from flask_restful import Api
 from werkzeug.utils import redirect, secure_filename
 
 from models import PictureInActivity, db, PictureInGarbageSpot, User
+from utils import token_required
 
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
 
@@ -17,9 +18,10 @@ def allowed_file(filename):
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# Upload File
-@file_routes_blueprint.route('/uploadFile', methods=['POST'])
-def upload_file():
+# Upload Profile File
+@file_routes_blueprint.route('/uploadProfileFile', methods=['POST'])
+@token_required
+def upload_profile_file(current_user):
     # Check if post request has file
     if 'file' not in request.files:
         flash('No file part')
@@ -33,49 +35,72 @@ def upload_file():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
 
-        folder_code = filename[0:3]
+        upload_folder = os.path.join(current_app.config['UPLOAD_FOLDER'], "profiles")
 
-        split_filename = filename.split("-")[0]
+        user = db.session.query(User).filter_by(id=current_user.id)
+        if not user:
+            return make_response("500 INTERNAL SERVER ERROR - User not found", 500)
+        user.icon = filename
 
-        try:
-            id = split_filename[3:]
-        except:
-            return make_response("500 INTERNAL SERVER ERROR - File name without source id", 500)
-
-        print("FILENAME: " + filename + "\nFOLDER CODE: " + folder_code + "\nID: " + id)
-
-        if folder_code == "201":
-            upload_folder = os.path.join(current_app.config['UPLOAD_FOLDER'], "activities")
-            new_activity_file = PictureInActivity(
-                activityID=id,
-                path=filename
-            )
-
-            db.session.add(new_activity_file)
-            db.session.commit()
-
-        elif folder_code == "202":
-            upload_folder = os.path.join(current_app.config['UPLOAD_FOLDER'], "garbagespots")
-            new_garbagespot_file = PictureInGarbageSpot(
-                garbageSpotID=id,
-                path=filename
-            )
-
-            db.session.add(new_garbagespot_file)
-            db.session.commit()
-
-        elif folder_code == "203":
-            upload_folder = os.path.join(current_app.config['UPLOAD_FOLDER'], "profiles")
-
-            user = db.session.query(User).filter_by(id=id)
-            if not user:
-                return make_response("500 INTERNAL SERVER ERROR - User id not found", 500)
-            user.icon = filename
-
-            db.session.commit()
-
-        else:
-            return make_response("500 INTERNAL SERVER ERROR - Invalid file name", 500)
+        db.session.commit()
 
         file.save(os.path.join(upload_folder, filename))
-        return make_response("200 OK - File saved", 200)
+        return make_response("200 OK - Profile file saved", 200)
+
+
+# Upload Activity File
+@file_routes_blueprint.route('/uploadActivityFile', methods=['POST'])
+@token_required
+def upload_activity_file(activity_id):
+    # Check if post request has file
+    if 'file' not in request.files:
+        return make_response("400 BAD REQUEST - No file part", 400)
+
+    file = request.files['file']
+    if file.filename == '':
+        return make_response("400 BAD REQUEST - No selected file", 400)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+
+        upload_folder = os.path.join(current_app.config['UPLOAD_FOLDER'], "activities")
+
+        new_activity_file = PictureInActivity(
+            activityID=id,
+            path=filename
+        )
+
+        db.session.add(new_activity_file)
+        db.session.commit()
+
+        file.save(os.path.join(upload_folder, filename))
+        return make_response("200 OK - Activity file saved", 200)
+
+
+# Upload Activity File
+@file_routes_blueprint.route('/uploadActivityFile', methods=['POST'])
+@token_required
+def upload_activity_file(activity_id):
+    # Check if post request has file
+    if 'file' not in request.files:
+        return make_response("400 BAD REQUEST - No file part", 400)
+
+    file = request.files['file']
+    if file.filename == '':
+        return make_response("400 BAD REQUEST - No selected file", 400)
+
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+
+        upload_folder = os.path.join(current_app.config['UPLOAD_FOLDER'], "garbagespots")
+        new_garbagespot_file = PictureInGarbageSpot(
+            garbageSpotID=id,
+            path=filename
+        )
+
+        db.session.add(new_garbagespot_file)
+        db.session.commit()
+
+        file.save(os.path.join(upload_folder, filename))
+        return make_response("200 OK - Activity file saved", 200)
+
