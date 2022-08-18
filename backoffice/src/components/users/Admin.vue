7937f09c150be5ff777883417f8388296b651039
Patricia <patricia9.2010@gmail.com>
2022-08-18 14:18:22 +0100

    back office details

---
+++ backoffice/src/components/users/Admin.vue
@@ -0,0 +1,145 @@
+<template>
+  <form class="row g-3 needs-validation" novalidate @submit.prevent="register">
+    <h3 class="mt-5 mb-3 text-light text-center">Criar Admin</h3>
+
+    <hr />
+    <div class="mb-3 mx-auto">
+      <div class="mb-3 w-25 mx-auto">
+        <label for="inputUsername" class="form-label text-light"
+          >Username</label
+        >
+        <input
+          type="text"
+          class="form-control"
+          id="inputUsername"
+          required
+          v-model="credentials.username"
+        />
+      </div>
+      <div class="mb-3 w-25 mx-auto">
+        <label for="inputPassword" class="form-label text-light"
+          >Password</label
+        >
+        <input
+          type="password"
+          class="form-control"
+          id="inputPassword"
+          required
+          v-model="credentials.password"
+        />
+      </div>
+      <div class="mb-3 w-25 mx-auto">
+        <label for="inputPasswordConfirmation" class="form-label text-light"
+          >Password Confirmation</label
+        >
+        <input
+          type="password"
+          class="form-control"
+          id="inputPasswordConfirmation"
+          required
+          v-model="credentials.passwordConfirmation"
+        />
+      </div>
+      <div class="mb-3 mx-auto">
+        <div class="mb-3 w-25 mx-auto">
+          <label for="inputName" class="form-label text-light">Name</label>
+          <input
+            type="text"
+            class="form-control"
+            id="inputName"
+            required
+            v-model="credentials.name"
+          />
+        </div>
+      </div>
+      <div class="mb-3 mx-auto">
+        <div class="mb-3 w-25 mx-auto">
+          <label for="inputEmail" class="form-label text-light">Email</label>
+          <input
+            type="email"
+            class="form-control"
+            id="inputEmail"
+            required
+            v-model="credentials.email"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="mb-3 d-flex justify-content-center">
+      <button type="button" class="btn btn-primary px-5" @click="validate">
+        Criar
+      </button>
+    </div>
+  </form>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "Admin",
+  data() {
+    return {
+      credentials: {
+        username: "",
+        name: "",
+        password: "",
+        passwordConfirmation: "",
+        email: "",
+      },
+      errors: null,
+    };
+  },
+  methods: {
+    validate() {
+      if (
+        this.credentials.email.length === 0 ||
+        this.credentials.name.length === 0 ||
+        this.credentials.password.length === 0 ||
+        this.credentials.passwordConfirmation.length === 0 ||
+        this.credentials.username.length === 0 
+      ) {
+        this.$toast.error("Campros precisam de ser preenchidos!");
+      } else {
+        if (
+          this.credentials.passwordConfirmation.length === 0 ||
+          this.credentials.password != this.credentials.passwordConfirmation
+        ) {
+            this.$toast.error("Passsword e Password conformation precisam de ser identicas");
+  
+           
+        } else {
+          if (
+            !this.credentials.email.match(
+              /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+            )
+          ) {
+              this.$toast.error("Email invalido");
+  
+          } else {
+            this.register();
+          }
+        }
+      }
+    },
+    async register() {
+      await axios
+        .post("registerAdmin", this.credentials)
+        .then(() => {
+          this.$toast.success(`Administrador ${this.credentials.name} foi criado.`);
+          this.$emit("register");
+          this.$router.push({
+            name: "Dashboard",
+          });
+        })
+        .catch((error) => {
+          this.$toast.error(`Cant register with these credentials! - ${error}}`);
+        });
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
